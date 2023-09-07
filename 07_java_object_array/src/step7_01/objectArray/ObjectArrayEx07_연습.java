b6b3e569aaa8d65b044fb8a97e249c29904c904b
fursiontwo <yr0955@naver.com>
2023-09-07 21:43:55 +0900

    Commit 230907

--- 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx07_연습.java
+++ 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx07_연습.java
@@ -2,18 +2,18 @@ package step7_01.objectArray;
 
 //# 클래스 배열 응용
 
-class Subject{
+class Subject_연습{
 	
-	String name;
-	int score;
+	String name;  // 과목명
+	int score;  // 점수
 	
 }
 
 
-class Student{
+class Student_연습{
 	
-	Subject[] subjects;	// Subject 객체를 담을 클래스 배열
-	String name;
+	Subject_연습[] subjects;  // 과목명과 점수 정보가 담긴 Subject_연습 객체
+	String name;  // 학생 이름
 	
 }
 
@@ -21,47 +21,50 @@ class Student{
 public class ObjectArrayEx07_연습 {
 
 	public static void main(String[] args) {
-
-		Student[] studentList = new Student[3];
 		
-		studentList[0] = new Student();
-		studentList[0].name = "학생0";
-		studentList[0].subjects = new Subject[3];
+		Student_연습[] studentList = new Student_연습[3];
 		
-		for (int i = 0; i < 3; i++) {
-			studentList[0].subjects[i] = new Subject();
-			studentList[0].subjects[i].name = "학생0 과목" + i;
+		for (int i = 0; i < studentList.length; i++) {
+			studentList[i] = new Student_연습();
+		}
+		
+		studentList[0].name = "학생1";
+		studentList[0].subjects = new Subject_연습[3];
+		
+		for (int i = 0; i < studentList[0].subjects.length; i++) {
+			studentList[0].subjects[i] = new Subject_연습();
+			studentList[0].subjects[i].name = "학생 0 과목 " + i; 
 			studentList[0].subjects[i].score = 100;
 		}
 		
-		studentList[1] = new Student();
-		studentList[1].name = "학생1";
-		studentList[1].subjects = new Subject[2];
-		studentList[1].subjects[0] = new Subject();
-		studentList[1].subjects[0].name = "학생1 과목0";
-		studentList[1].subjects[0].score = 100;
+		studentList[1].name = "학생2";
+		studentList[1].subjects = new Subject_연습[2];
+		for (int i = 0; i < studentList[1].subjects.length; i++) {
+			studentList[1].subjects[i] = new Subject_연습();
+			studentList[1].subjects[i].name = "학생 1 과목 " + i;
+		}
+		studentList[1].subjects[0].score = 80;
+		studentList[1].subjects[1].score = 75;
 		
-		studentList[1].subjects[1] = new Subject();
-		studentList[1].subjects[1].name = "학생1 과목1";
-		studentList[1].subjects[1].score = 100;
-		
-		studentList[2] = new Student();
-		studentList[2].name = "학생2";
-		studentList[2].subjects = new Subject[1];
-		studentList[2].subjects[0] = new Subject();
-		studentList[2].subjects[0].name = "학생2 과목1";
-		studentList[2].subjects[0].score = 100;
-		
-		
+		studentList[2].name = "학생3";
+		studentList[2].subjects = new Subject_연습[4];
+		for (int i = 0; i < studentList[2].subjects.length; i++) {
+			studentList[2].subjects[i] = new Subject_연습();
+			studentList[2].subjects[i].name = "학생 2 과목 " + i;
+		}
+		studentList[2].subjects[0].score = 10;
+		studentList[2].subjects[1].score = 30;
+		studentList[2].subjects[2].score = 50;
+		studentList[2].subjects[3].score = 70;
 		
 		for (int i = 0; i < studentList.length; i++) {
-			System.out.println("--- " + studentList[i].name + " ---");
+			System.out.println("-".repeat(3) + "학생" + i + "-".repeat(3));
 			for (int j = 0; j < studentList[i].subjects.length; j++) {
-				System.out.println(studentList[i].subjects[j].name  + " / " + studentList[i].subjects[j].score);
+				System.out.println(studentList[i].subjects[j].name + " / " + studentList[i].subjects[j].score);
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 }
